4ca8690696bd4558e4675399ad82ad56a715e121
shiv <suprememind@live.com>
2022-05-13 17:45:08 +0530

    .

---
+++ ConsoleTester/Program.fs
@@ -0,0 +1,13 @@
+﻿open WebCrawlerFsLib.WebCrawler
+open System.Threading.Tasks
+open System
+
+let url = "https://eclipse2024.org"
+
+setBaseUrl url
+consumer() |> Async.Start
+linksProducer ("", url) |> Async.RunSynchronously |> ignore
+
+Console.ReadLine()|>ignore
+
+
