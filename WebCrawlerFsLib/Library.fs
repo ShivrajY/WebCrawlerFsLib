d01cd653f99dd53facc742d51d62ea72bfbe00e6
shiv <shivrajy@gmail.com>
2022-05-13 17:22:02 +0530

    .

--- WebCrawlerFsLib/Library.fs
+++ WebCrawlerFsLib/Library.fs
@@ -2,6 +2,7 @@
 module WebCrawlerFsLib.WebCrawler
 
 open System
+open System.Linq
 open System.Net
 open System.Net.Http
 open System.IO
@@ -15,6 +16,22 @@ open AngleSharp.Io.Network
 open AngleSharp.Html.Parser
 
 let LIMIT = 100
+
+type RequestGate(n:int) =
+    let semaphore = new Semaphore(initialCount=n, maximumCount = n)
+    member _.AsyncAcquire(?timeout) = 
+        async{
+                let! ok = Async.AwaitWaitHandle(semaphore, ?millisecondsTimeout = timeout)
+                if (ok) then
+                   return {
+                            new System.IDisposable with 
+                                member _.Dispose() = semaphore.Release()|>ignore
+                           }
+                else  
+                    return! failwith "Couldn't aquire semaphore"    
+        }
+
+let webRequestGate = RequestGate(10)
 
 ServicePointManager.ServerCertificateValidationCallback <- (fun _ _ _ _ -> true)
 ServicePointManager.DefaultConnectionLimit <- LIMIT
@@ -37,47 +54,81 @@ let config = Configuration.Default
                 .WithJs()
 
 let linksDictionary = new ConcurrentDictionary<string,string>()             
-let blockingCollection = new BlockingCollection<string>()
+let blockingCollection = new BlockingCollection<string*string>()
 
-let linksProducer (url:string) =
-    task {
+let mutable BaseUrl = "" 
 
+let setBaseUrl baseUrl = BaseUrl <- baseUrl
+                         httpClient.BaseAddress <- Uri(baseUrl)
+
+let log msg = printfn "%A" msg
+let saveToFiles (data:(bool * string * string)) = 
+    log "Saving file..."
+    log data
+let linksProducer (parentUrl:string, url:string) =
+    async{
+        
+
+        if not (linksDictionary.ContainsKey(url)) then
+          linksDictionary.[url] <- parentUrl
+          log "Inside producer"
           try
-            use! response = httpClient.GetAsync(url, HttpCompletionOption.ResponseHeadersRead)
+            log ("Link: " + url)
+            use! response = httpClient.GetAsync(url, HttpCompletionOption.ResponseHeadersRead)|>Async.AwaitTask
             if(response.IsSuccessStatusCode) then
-                let contentType = response.Content.Headers.ContentType.MediaType
-                if(contentType = "text/html") then
-                    let! source = response.Content.ReadAsStringAsync()        
-                    use context = BrowsingContext.New(config)
-                    let parser = context.GetService<IHtmlParser>()
-                    use! doc = parser.ParseDocumentAsync(source)
-                    let links = 
-                             doc.QuerySelectorAll("a")
-                             |> Seq.filter(fun x -> x.HasAttribute("href"))
-                             |> Seq.map (fun x -> x.GetAttribute("href"))
+                log ("Successful")
+                if(url.StartsWith("http") && not (url.Contains(BaseUrl))) then
+                    //save file
+                    saveToFiles (true, parentUrl, url)
+                else
+                    let contentType = response.Content.Headers.ContentType.MediaType
+                    log contentType
+                    if(contentType = "text/html") then
+                        let! source = response.Content.ReadAsStringAsync() |>Async.AwaitTask       
+                        use context = BrowsingContext.New(config)
+                        let parser = context.GetService<IHtmlParser>()
+                        use! doc = parser.ParseDocumentAsync(source)|>Async.AwaitTask
+                        let links = 
+                                 doc.QuerySelectorAll("a")
+                                 |> Seq.filter(fun x -> x.HasAttribute("href"))
+                                 |> Seq.map (fun x -> x.GetAttribute("href"))
+                        log ("Total Links: "+ links.Count().ToString())
+                        for link in links do
+                              let newLink =
+                                            if(link.StartsWith("/")) then
+                                                $"{BaseUrl}{link}"
+                                            else if not (link.StartsWith("http")) then
+                                                $"{BaseUrl}/{link}"
+                                            else link
+                              blockingCollection.Add((url,newLink))
+                        //save file
 
-                    for link in links do
-                        if not (linksDictionary.ContainsKey(link)) then
-                          linksDictionary.[link] <- url
-                          blockingCollection.Add(link)
-                    return true
-                else return true
-            else return false
+                        saveToFiles (true, parentUrl, url)
+                     else //Content type is not html but successful
+                              //save file                 
+                         saveToFiles (true,parentUrl, url)
+            else 
+                  //save file
+                  saveToFiles (false, parentUrl, url)
           with
-            |_ ->  return false
-
+            |_ ->  //save file
+                   saveToFiles (false,parentUrl, url)
         }
-    
+
 let consumer() =
-    task{
-           for url in blockingCollection.GetConsumingEnumerable() do
-              let! result = linksProducer url   
-              if (result) then () 
-                //write to the good file
-              else ()
-                //write to the bad file
+        async{
+               log "Inside consumer"
+               let taskList = ResizeArray()
+               for (parentUrl, url) in blockingCollection.GetConsumingEnumerable() do
+                    taskList.Add(linksProducer (parentUrl,url))
+                    if(taskList.Count > 10) then
+                        taskList.ToArray()|>Async.Parallel|>Async.RunSynchronously|>ignore
+                    
+            }
 
-        }
+
+       
+   
 
         
 
